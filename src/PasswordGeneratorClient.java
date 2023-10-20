8c5efab19d3cba3202789eb41c42ded3bc379c26
Joey Villafuerte <80644396+JoeyVillafuerte@users.noreply.github.com>
2023-10-19 23:56:06 -0400

    Update PasswordGeneratorClient.java

--- src/PasswordGeneratorClient.java
+++ src/PasswordGeneratorClient.java
@@ -11,64 +11,58 @@ public class PasswordGeneratorClient {
         String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
 
-        try (Socket socket = new Socket(hostName, portNumber);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in)) ) {
+        try {
+            Socket socket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
             String userInput;
 
             while (true) {
                 System.out.print("Type 'generate' to request a password (or 'exit' to quit): ");
                 userInput = stdIn.readLine();
-                out.println(userInput);
 
+                // Notify the server about the user disconnecting
                 if (userInput.equalsIgnoreCase("exit")) {
-                    // Exit the loop and terminate the client
-                    System.out.println("User has disconnected.");
+                    out.println("exit");
                     break;
                 } else if (userInput.equalsIgnoreCase("generate")) {
                     System.out.println("\nPlease fill in the following information to get your new password.");
                     System.out.print("Password Length (please enter a number in digits): ");
-                    int length;
-                    try {
-                        length = Integer.parseInt(stdIn.readLine());
-                    } catch (NumberFormatException e) {
-                        System.out.println("Invalid input. Please enter a valid number for password length.");
-                        continue;
-                    }
-
-                    boolean includeUppercase, includeLowercase, includeNumbers, includeSymbols;
-
-                    // Ask the user for password criteria
+                    int length = Integer.parseInt(stdIn.readLine());
                     System.out.print("Include Uppercase (Y/N): ");
-                    includeUppercase = stdIn.readLine().equalsIgnoreCase("Y");
+                    boolean includeUppercase = stdIn.readLine().equalsIgnoreCase("Y");
                     System.out.print("Include Lowercase (Y/N): ");
-                    includeLowercase = stdIn.readLine().equalsIgnoreCase("Y");
+                    boolean includeLowercase = stdIn.readLine().equalsIgnoreCase("Y");
                     System.out.print("Include Numbers (Y/N): ");
-                    includeNumbers = stdIn.readLine().equalsIgnoreCase("Y");
+                    boolean includeNumbers = stdIn.readLine().equalsIgnoreCase("Y");
                     System.out.print("Include Symbols (Y/N): ");
-                    includeSymbols = stdIn.readLine().equalsIgnoreCase("Y");
+                    boolean includeSymbols = stdIn.readLine().equalsIgnoreCase("Y");
 
+                    // Check if all options are "No"
                     if (!includeUppercase && !includeLowercase && !includeNumbers && !includeSymbols) {
                         System.out.println("At least one option must be selected for password generation.");
-                        continue;
+                        continue; // Continue the loop to re-enter options
                     }
 
                     // Send the user's criteria to the server
                     out.println("generate " + length + " " + includeUppercase + " " + includeLowercase + " " + includeNumbers + " " + includeSymbols);
 
-                    // Receive and display the generated password and its strength from the server
+                    // Receives and displays the generated password and its strength from the server
                     String generatedPassword = in.readLine();
                     String passwordStrengthLabel = in.readLine();
 
                     // Outputs the new password and the calculated strength from the server
-                    System.out.println("\nGenerated Password: " + generatedPassword);
-                    System.out.println("Your generated password strength is: " + passwordStrengthLabel);
+                    System.out.println("Generated Password: " + generatedPassword);
+                    System.out.println("Your generated password strength is: " + passwordStrengthLabel + "\n");
                 } else {
                     System.out.println("Unknown command. Type 'generate' to request a password or 'exit' to quit.");
                 }
             }
+
+            // Close the client socket
+            socket.close();
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
